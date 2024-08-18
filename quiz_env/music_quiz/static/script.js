6d5136a16ff1943983025847911b83e5d44af3bf
Fijantos <finaltos@g-mail.com>
2024-08-18 11:08:34 +0200

    some css changes and wait is now timed to 429 error

--- quiz_env/music_quiz/static/script.js
+++ quiz_env/music_quiz/static/script.js
@@ -14,15 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
   // Funktion zum Abrufen der Fragen
   const fetchQuestions = async () => {
     try {
-      const response = await fetch(apiUrl); // Einmalig und dann muss man auch nicht 5 sekunden so warten
+      let response = await fetch(apiUrl); // Einmalig und dann muss man auch nicht 5 sekunden so warten
       if (!response.ok) {
         const errorText = await response.text(); // Detaillierte Fehlermeldung
+        if (response.status === 429) {
+          console.warn("Zu viele Anfragen. Wartezeit...");
+          await wait(5000); // Warte 5 Sekunden
+          return fetchQuestions(); // Versuch es erneut
+        }
         throw new Error(
           `Network response was not ok. Status: ${response.status} ${response.statusText}. Response: ${errorText}`
         );
       }
 
-      const data = await response.json();
+      let data = await response.json();
 
       if (data.response_code === 5) {
         throw new Error(
@@ -75,15 +80,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
-  // Verzögerung vor dem Abrufen der Fragen
-  const startFetching = async () => {
-    console.log("Warte 5 Sekunden, bevor die Fragen abgerufen werden...");
-    await wait(5000); // Warte 5 Sekunden
-    await fetchQuestions(); // Dann Fragen abrufen
-  };
-
   // Aufruf der Startfunktion
-  startFetching();
+  fetchQuestions();
 });
 
 // Funktion zur Validierung der Antworten
